c0b5990b76798b25ced571a9763927e86d0e2309
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-01-21 23:57:05 +0545

    initial commit

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
-@app.get("/")
+@app.get("/api")
 async def read_root():
-    return {"Hello": "World"}
+    return {"status": True, "message": "API Server hit SUCCESSFULLY!"}
