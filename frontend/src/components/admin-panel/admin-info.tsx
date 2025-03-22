7ce1db3054661fbae205d574b5dd7ae48c888480
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-03-22 22:32:54 +0545

    initial commit

--- frontend/src/components/admin-panel/admin-info.tsx
+++ frontend/src/components/admin-panel/admin-info.tsx
@@ -2,7 +2,7 @@ import {Avatar} from "@/components/ui/avatar.tsx";
 import {AvatarFallback, AvatarImage} from "@radix-ui/react-avatar";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover.tsx";
 import {Button} from "@/components/ui/button.tsx";
-import {ChevronsUpDown, LogOut, SquarePlus} from "lucide-react";
+import {BadgeCheck, Bell, BookHeart, ChevronsUpDown, LogOut, Sparkle} from "lucide-react";
 import {Card, CardDescription} from "@/components/ui/card.tsx";
 import {Separator} from "@/components/ui/separator.tsx";
 
@@ -10,19 +10,19 @@ const AdminInfo = () => {
     return (
         <Popover>
             <PopoverTrigger>
-                <div className="flex space-x-5 items-center w-full border-black py-2 border-2 rounded justify-center bg-blue-300">
+                <div className="flex text-white space-x-5 items-center w-full border-black py-2 border-2 rounded justify-center bg-blue-700">
                     <Avatar>
                         <AvatarImage src="https://github.com/shadcn.png" />
-                        <AvatarFallback>CN</AvatarFallback>
+                        <AvatarFallback>Admin</AvatarFallback>
                     </Avatar>
                     <div>
-                        <p className="font-bold text-slate-700">Nabin Purbey</p>
+                        <p className="font-bold">Nabin Purbey</p>
                         <p className="text-sm">Admin</p>
                     </div>
                     <p><ChevronsUpDown /></p>
                 </div>
             </PopoverTrigger>
-            <PopoverContent className="w-[200px] ml-64 shadow-2xl p-0 flex flex-col">
+            <PopoverContent className="w-[250px] ml-64 shadow-2xl p-0 flex flex-col">
                 <Card>
                     <CardDescription className="text-center mb-1">Admin Profile</CardDescription>
                     <Separator />
@@ -36,9 +36,14 @@ const AdminInfo = () => {
                             <p className="text-sm">Admin</p>
                         </div>
                     </div>
-                    <Separator />
-
-                    <Button variant="outline" className="w-full flex justify-start"><LogOut /> Logout</Button>
+                    <Separator className="my-1"/>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300 border-0"><Sparkle /> Upgrade To Pro</Button>
+                    <Separator className="my-1"/>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300 border-0"><BadgeCheck /> Account</Button>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300 border-0"><Bell /> Notification</Button>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300 border-0"><BookHeart /> Personalize</Button>
+                    <Separator className="my-1"/>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300 border-0"><LogOut /> Logout</Button>
                 </Card>
             </PopoverContent>
         </Popover>
--- frontend/src/components/admin-panel/org-info.tsx
+++ frontend/src/components/admin-panel/org-info.tsx
@@ -1,4 +1,4 @@
-import {Building2, ChevronsUpDown, SquarePlus} from "lucide-react";
+import {Building2, ChevronsUpDown, MessageCircleCode, SquarePlus, Users} from "lucide-react";
 import {Popover, PopoverContent, PopoverTrigger} from "@/components/ui/popover.tsx";
 import {Button} from "@/components/ui/button.tsx";
 import {Card, CardDescription} from "@/components/ui/card.tsx";
@@ -8,21 +8,23 @@ const OrgInfo = () => {
     return (
         <Popover>
             <PopoverTrigger>
-                <div className="flex justify-center items-center bg-blue-300 px-1 border-2 border-black rounded py-2">
-                    <div className="bg-blue-700 rounded text-white py-2 px-2"><Building2 /></div>
+                <div className="flex justify-center text-white items-center bg-blue-700 px-1 border-2 border-black rounded py-2">
+                    <div className="w-full h-[50px] basis-1/3">
+                        <img src="/assets/images/emblem_of_nepal.svg" alt="Emblem Of Nepal" className="w-full h-full object-fill"/>
+                    </div>
                     <div>
-                        <p className="font-bold text-slate-700">Railway Ticket Reservation System</p>
+                        <p className="font-bold">Railway Ticket Reservation System</p>
                     </div>
                     <p><ChevronsUpDown /></p>
                 </div>
             </PopoverTrigger>
-            <PopoverContent className="w-[200px] ml-64 shadow-2xl p-0 flex flex-col">
+            <PopoverContent className="w-[250px] ml-64 shadow-2xl p-0 flex flex-col">
                 <Card className="py-0">
                     <CardDescription className="text-center mb-1">Team</CardDescription>
                     <Separator />
-                    <Button variant="outline" className="w-full flex justify-start"><SquarePlus />Team Members</Button>
-                    <Button variant="outline" className="w-full flex justify-start"><SquarePlus />Add Admin</Button>
-                    <Button variant="outline" className="w-full flex justify-start"><SquarePlus />Add Team</Button>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300"><Users /> Team Members</Button>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300"><SquarePlus /> Add Admin</Button>
+                    <Button variant="outline" className="rounded-none w-full flex justify-start hover:bg-gray-300"><MessageCircleCode /> Feedbacks</Button>
                 </Card>
             </PopoverContent>
         </Popover>
