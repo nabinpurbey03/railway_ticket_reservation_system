034da316afded4764ea2818a96a22cbd5fc89ede
Nabin Purbey <nabinpurbey03@hotmail.com>
2025-06-17 18:34:06 +0545

    initial commit

---
+++ backend/src/admin.py
@@ -0,0 +1,24 @@
+import os
+
+import psycopg2
+from dotenv import load_dotenv
+
+
+class Admin:
+    def __init__(self):
+        load_dotenv()
+        self.__conn = psycopg2.connect(os.getenv("DATABASE_URL"))
+        self.__cur = self.__conn.cursor()
+
+    def show_all_tickets(self):
+        sql = '''
+        SELECT journey_date FROM ticket where journey_date between CURRENT_DATE AND CURRENT_DATE + 8;
+        '''
+        self.__cur.execute(sql)
+        tickets = self.__cur.fetchall()
+        print(tickets)
+
+
+
+a = Admin()
+a.show_all_tickets()
